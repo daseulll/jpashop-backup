bcb2c118d33b09b1267bf4fb0efe14ec83333899
daseulll <nldaseul@gmail.com>
2022-06-29 09:51:57 +0900

    tmp

--- src/test/java/jpabook/jpashop/repository/MemberRepositoryTest.java
+++ src/test/java/jpabook/jpashop/repository/MemberRepositoryTest.java
@@ -5,11 +5,13 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
 
 @SpringBootTest
 @Transactional
+@Rollback(false)
 class MemberRepositoryTest {
 
     @Autowired MemberRepository memberRepository;
@@ -24,6 +26,7 @@ class MemberRepositoryTest {
 
         Assertions.assertThat(member.getId()).isEqualTo(foundMember.getId());
         Assertions.assertThat(foundMember.getUsername()).isEqualTo("newName");
+        Assertions.assertThat(foundMember).isEqualTo(member);
 
 
     }
